94de66b66e57f5758e6c415cfc77c1e44d40a2b2
Yashu7 <janmalawski2@gmail.com>
2024-09-23 20:48:30 +0200

    UserDAOService, abstract class with connectionString

---
+++ MediProjectWebApp/Helpers/ConnectionStringHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MediProjectWebApp.Helpers
+{
+    public abstract class ConnectionStringHelper
+    {
+        public string connString = ConfigurationManager.ConnectionStrings["MedDBConnection"].ConnectionString;
+    }
+}
---
+++ MediProjectWebApp/Services/UserDAOService.cs
@@ -0,0 +1,18 @@
+﻿using MediProjectWebApp.Helpers;
+using MediProjectWebApp.Models.Accounts;
+using MediProjectWebApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediProjectWebApp.Services
+{
+    public class UserDAOService : ConnectionStringHelper, IUserDaoService
+    {
+        public QueryStatus LoginUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
