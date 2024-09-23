88e631d819dc8ae307eb8425f913f742a6de90c0
Yashu7 <janmalawski2@gmail.com>
2024-09-23 20:44:53 +0200

    New test class for Login, added service interface for UserDAO

---
+++ MediProjectWebApp/Services/Interfaces/IUserDaoService.cs
@@ -0,0 +1,15 @@
+﻿using MediProjectWebApp.Helpers;
+using MediProjectWebApp.Models.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediProjectWebApp.Services.Interfaces
+{
+    public interface IUserDaoService
+    {
+        QueryStatus LoginUser(User user);
+    }
+}
