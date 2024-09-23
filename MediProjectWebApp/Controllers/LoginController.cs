fbcb62ac2777fd2dbca1c7115e8890147b9318a1
Yashu7 <janmalawski2@gmail.com>
2024-09-23 22:58:03 +0200

    Added blowfish helper, encrypt password, login method

--- MediProjectWebApp/Controllers/LoginController.cs
+++ MediProjectWebApp/Controllers/LoginController.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿using MediProjectWebApp.Helpers;
+using MediProjectWebApp.Models.Accounts;
+using MediProjectWebApp.Services;
+using MediProjectWebApp.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MediProjectWebApp.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        [AllowAnonymous]
         public ActionResult Index()
         {
+            FormsAuthentication.SignOut();
             return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var encryptedPassword = BlowfishHelper.EncryptBlowfish(model.Password, "12345678abcdefgmypassword");
+                IUserDaoService userDaoService = new UserDAOService();
+                var queryStatus = userDaoService.LoginUser(model);
+                if (queryStatus == QueryStatus.Success)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
+            return Json(new { success = false, message = "Please enter valid login credentials." });
         }
     }
 }
---
+++ MediProjectWebApp/Helpers/BlowfishHelper.cs
@@ -0,0 +1,30 @@
+﻿using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MediProjectWebApp.Helpers
+{
+    public static class BlowfishHelper
+    {
+        public static string EncryptBlowfish(string plainText, string key)
+        {
+            var engine = new BlowfishEngine();
+            var blockCipher = new PaddedBufferedBlockCipher(engine);
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+
+            blockCipher.Init(true, new KeyParameter(keyBytes));
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] outputBytes = new byte[blockCipher.GetOutputSize(inputBytes.Length)];
+            int length = blockCipher.ProcessBytes(inputBytes, 0, inputBytes.Length, outputBytes, 0);
+            blockCipher.DoFinal(outputBytes, length);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
